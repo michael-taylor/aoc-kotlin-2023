36b8c7ed9fffdf1f646658a69862577a1d0d43a2
Michael R. Taylor <michael@atomic.city>
2023-12-04 00:14:29 -0600

    Added a utility function that might come in handy later

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,26 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Return the value of a whole number given an index to a digit in that number.inside a string
+ *
+ * This function will return null if the index doesn't point to a digit
+ */
+fun getWholeNumberAtIndexOfString(text: String, index: Int): Int? {
+    if (!text[index].isDigit()) return null
+
+    var startIndex = index
+    while ((startIndex >= 0) && (text[startIndex].isDigit())) {
+        startIndex--
+    }
+    startIndex++ // Rewind one for that last decrement
+
+    var stopIndex = index
+    while ((stopIndex < text.length) && (text[stopIndex].isDigit())) {
+        stopIndex++
+    }
+    // No need to rewind stopIndex because it is exclusive in the range
+
+    return text.substring(startIndex, stopIndex).toInt()
+}
